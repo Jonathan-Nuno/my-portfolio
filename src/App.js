f53146fc2b1c239b28caa627b04aeb79929dac65
Jonathan Nuno <jnuno.423@gmail.com>
2020-11-20 16:36:04 -0500

    added about, contact and home pages along with routers being setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Rout, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import './App.css';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap'
 
 import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
 
 
 // converted from function to class because we are using states
@@ -51,10 +54,14 @@ class App extends React.Component {
               <Nav className="nl-auto">
                 <Link className='nav-link' to='/'>Home</Link>
                 <Link className='nav-link' to='/about'>About</Link>
-                <Link className='nav-link' to='/contact'>Contact</Link>
+                <Link className='nav-link' to='/contact'>Contact</Link>                              
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          {/* 'exact' is used so the path='/' doesn't work for all 3 routes technically */}
+          <Route path='/' exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path='/about' render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path='/contact' render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
         

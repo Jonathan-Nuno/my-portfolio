b70419c958d49f64db4f41dd545bc2d5ff3a3b3e
Jonathan Nuno <jnuno.423@gmail.com>
2020-11-20 14:38:05 -0500

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Rout, Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap'
+
+import Footer from './components/Footer'
+
 
 // converted from function to class because we are using states
 class App extends React.Component {
@@ -30,7 +37,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello From React</div>
+      <Router>
+        {/* set fluid to true for container to take width of screen */}
+        <Container className='p-0' fluid={true}>
+
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand>Jonathan Nuno</Navbar.Brand>
+
+            {/* aria-controls is used for accessability */}
+            <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
+            {/* navbar.collapse is used to make a dropdown based on page size */}
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className="nl-auto">
+                <Link className='nav-link' to='/'>Home</Link>
+                <Link className='nav-link' to='/about'>About</Link>
+                <Link className='nav-link' to='/contact'>Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Footer />
+        
+        </Container>
+      </Router>
     );
   }
 }

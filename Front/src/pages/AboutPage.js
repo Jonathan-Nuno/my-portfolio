07711c4a6dcd477d190d2a508794701fb289ac64
Jonathan Nuno <jnuno.423@gmail.com>
2020-11-21 22:22:38 -0500

    text edit

--- Front/src/pages/AboutPage.js
+++ Front/src/pages/AboutPage.js
@@ -9,11 +9,11 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-                <p>Hello, my name is Jonathan. I'm a full stack software engineer with experince in React, Express.js, Node.js, PostgreSQL and MongoDB.</p>
+                <p>Hello, my name is Jonathan. I'm a full stack software engineer with experince in React, Express.js, Node.js, PostgreSQL and Firebase.</p>
 
                 <p>I'm always learning and always improving by aqcuiring new skills and gaining additional experince in my existing skillset. Currently, I'm working on improving my React experince.</p>
                 
-                <p>My latest project invlovled building my portfolio using React and setting up a backend to host my contact form submissions. My portfolio was built using React, Bootstrap, MongoDB and Node.js.</p>
+                <p>My latest project invlovled building my portfolio using React and setting up a backend to host my contact form submissions. My portfolio was built using React, Express.js, Bootstrap, Firebase and Node.js.</p>
                 
                 <p>When I'm not learning or working on a new project, you can find me in the kitchen testing new recipes. I do enjoy the prepartion, testing and execution required to whip up a delicious meal!</p>
 

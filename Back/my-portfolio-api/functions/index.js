b919303b2dc9a7cfb4c724d537a17c738b3c694c
Jonathan Nuno <jnuno.423@gmail.com>
2020-11-24 14:23:27 -0500

    firebase backend added and deployed

--- Back/my-portfolio-api/functions/index.js
+++ Back/my-portfolio-api/functions/index.js
@@ -1,12 +1,13 @@
+const functions = require('firebase-functions');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
 
-const sendGrid = require('@sendGrid/mail');
-const SG_API_KEY = process.env.SENDGRID_API_KEY;
-const PE = process.env.Personal_Email;
-const PORT = process.env.PORT;
+const sendGrid = require('@sendgrid/mail');
+const SG_API_KEY = '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';
+const PE = 'jnuno.423@gmail.com';
+const PORT = 5000;
 
 const app = express();
 
@@ -16,11 +17,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server only
-    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
     next();
 });
+
+app.get('/', (req, res, next) => {
+    res.send('go to API')
+})
 
 app.get('/api', (req, res, next) => {
     res.send('API Status: Running')
@@ -42,19 +47,21 @@ app.post('/api/email', (req, res, next) => {
             res.status(200).json({
                 success: true
             });
+            return null;
         })
         .catch(err => {
-            
+
             console.log('error: ', err);
             res.status(401).json({
                 success: false
             });
-
+            return null;
         });
 
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running...')
+    console.log(`Server is running...Port ${PORT}`)
 });
 
+exports.app = functions.https.onRequest(app);
